562b7837717c6714140af045b6fbf76a5c23a822
Patchfox <mario.schuetzle@outlook.de>
2020-08-18 21:19:10 +0200

    first init

---
+++ end/gatsby-config.js
@@ -0,0 +1,29 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+
+    
+
+
+    // Algolia
+
+
+    // Typography
+
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+
+
+
+  ],
+}
